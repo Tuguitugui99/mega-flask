df0307f8e75e9aedcf10bc3cb28676d7c0bf0a75
sara <sara.alicia.fp@gmail.com>
2023-05-04 15:59:28 -0400

    se agrega formulario

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template, request 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def hello_barbie():
+    return render_template ("index.html")
+
+@app.route('/')
+def hola-que-tal(): 
+        name = request.args.get("username")
+        lastname = request.args.get("lastname")
+
+return render_template ("/reels.html",name=name,lastname=lastname)    
